1800c0bf684339a344eb642e3d6fd833f8cf2dd7
Lilian Maurin <lilianmaurin.wcs2019@gmail.com>
2020-10-25 17:42:23 +0100

    Add proper page titles

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,5 +1,7 @@
 @extends('app')
 
+@section('title', 'About Us | Hello City')
+
 @section('content')
     <p>&copy; Built with &hearts; by <a href="https://unevireesudamericaine.wordpress.com/" target="_blank">Lilian Maurin</a>.</p>
     <p>
