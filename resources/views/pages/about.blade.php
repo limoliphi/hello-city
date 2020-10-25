2688b788bb82e1a9292a2749dbf477619fafdd8c
Lilian Maurin <lilianmaurin.wcs2019@gmail.com>
2020-10-25 17:02:49 +0100

    Remove duplication by using app layout

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,18 +1,8 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('app')
 
-        <title>About us | Hello City</title>
-
-    </head>
-    <body>
-        <p>&copy; Built with &hearts; by <a href="https://unevireesudamericaine.wordpress.com/" target="_blank">Lilian Maurin</a>.</p>
-        <p><a href="/">Revenir à la page d'accueil</a></p>
-
-        <footer>
-            <p>&copy; Copyright {{ date('Y') }}</p>
-        </footer>
-    </body>
-</html>
+@section('content')
+    <p>&copy; Built with &hearts; by <a href="https://unevireesudamericaine.wordpress.com/" target="_blank">Lilian Maurin</a>.</p>
+    <p>
+        <a href="/">Revenir à la page d'accueil</a>
+    </p>
+@endsection
