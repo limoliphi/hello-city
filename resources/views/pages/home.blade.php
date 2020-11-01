93c0cd26d881140f17fb2971112a41c2073f6029
Lilian Maurin <lilianmaurin.wcs2019@gmail.com>
2020-11-01 16:22:15 +0100

    Add images

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,6 +3,7 @@
 @section('title', 'Hello City')
 
 @section('content')
+    <img src="/images/quebec-flag.png" alt="Quebec flag">
     <h1>Hello from Québec... </h1>
 
     <p>It's currently {{ date('h:i A') }}.</p>
