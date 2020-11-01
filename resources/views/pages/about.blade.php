e8cee242631485d2d8e50d5f49b2ef14ee1486df
Lilian Maurin <lilianmaurin.wcs2019@gmail.com>
2020-11-01 17:33:04 +0100

    Add style with Tailwind CSS

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -3,9 +3,10 @@
 @section('title', 'About Us | ' . config('app.name'))
 
 @section('content')
-    <img src="{{ asset('images/image-about.jpg') }}" alt="App logo">
-    <p>&copy; Built with &hearts; by <a href="https://unevireesudamericaine.wordpress.com/" target="_blank">Lilian Maurin</a>.</p>
+    <img src="{{ asset('images/image-about.jpg') }}" alt="App logo" class="my-12 rounded shadow-md h-40">
+    <h2 class="mb-5 text-gray-700">
+        &copy; Built with <span class="text-pink-500">&hearts;</span> by <a href="https://unevireesudamericaine.wordpress.com/" target="_blank">Lilian Maurin</a>.</h2>
     <p>
-        <a href="{{ route('home') }}">Revenir à la page d'accueil</a>
+        <a href="{{ route('home') }}" class="text-indigo-500 hover:text-indigo-600 underline">Revenir à la page d'accueil</a>
     </p>
 @endsection
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,8 +3,8 @@
 @section('title', 'Hello City')
 
 @section('content')
-    <img src="{{ asset('images/quebec-flag.png') }}" alt="Quebec flag">
-    <h1>Hello from Québec... </h1>
+    <img src="{{ asset('images/quebec-flag.png') }}" alt="Quebec flag" class="mt-12 rounded shadow-md h-32">
+    <h1 class="mt-5 text-3xl sm:text-5xl font-semibold text-indigo-600">Hello from Québec... </h1>
 
-    <p>It's currently {{ date('h:i A') }}.</p>
+    <p class="text-lg text-gray-800">It's currently {{ date('h:i A') }}.</p>
 @endsection
