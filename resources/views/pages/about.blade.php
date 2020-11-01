9be97e39c9789f7d227d089e1c67aec155980936
Lilian Maurin <lilianmaurin.wcs2019@gmail.com>
2020-11-01 16:41:00 +0100

    Make use of asset function help to load images

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'About Us | ' . config('app.name'))
 
 @section('content')
-    <img src="/images/image-about.jpg" alt="App logo">
+    <img src="{{ asset('images/image-about.jpg') }}" alt="App logo">
     <p>&copy; Built with &hearts; by <a href="https://unevireesudamericaine.wordpress.com/" target="_blank">Lilian Maurin</a>.</p>
     <p>
         <a href="{{ route('home') }}">Revenir à la page d'accueil</a>
