ebb8f8605b2df90cf93ab856c740a3366e3b16d3
Lilian Maurin <lilianmaurin.wcs2019@gmail.com>
2020-11-06 09:36:13 +0100

    Add page_title helper function and _footer partial and use them in app layout

---
+++ app/helpers.php
@@ -0,0 +1,9 @@
+<?php
+
+if (!function_exists('page_title')) {
+    function page_title(?string $pageTitle = null): string {
+        return $pageTitle
+            ? $pageTitle . ' | ' .config('app.name')
+            : config('app.name');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,3 @@ Route::get('/', function () {
 Route::get('/about-us', function () {
     return view('pages/about');
 })->name('about');
-
-Route::get('/help', function () {
-    return view('pages.help');
-});
